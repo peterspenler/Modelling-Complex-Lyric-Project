73c7d22f93d8e9193d8e56d470f26febc074e002
Blair Thompson <bthomp10@uoguelph.ca>
2019-03-17 14:44:56 +0000

    Added functino to remove useless chars and pad newlines and commas

--- concatenate_data.py
+++ concatenate_data.py
@@ -9,6 +9,17 @@ def clear_whitelines(data):
 	result = result.strip()
 	return result
 
+def prepare_chars(data):
+    result = data
+    result = result.replace('\n', ' \n ')
+    result = result.replace(',', ' ,')
+    
+    #remove characters that are not: alphanumeric, comma, apostrophe space, newline
+    result = [c for c in result if c.isalpha() or c == "'" or c == '\n' or c == " "or c == ","]
+    result = ''.join(result)
+    
+    return result
+
 corpus = open('corpus.lyc', 'w')
 
 total = len(os.listdir(directory))
@@ -18,6 +29,7 @@ for filename in os.listdir(directory):
 	print("Adding " + str(count) + '/' + str(total) + " to corpus", end='\r')
 	f = open("test_data/" + filename, 'r')
 	data = clear_whitelines(f.read())
+    data = prepare_chars(data)
 	if data.count('\n') >= 14:
 		corpus.write(data + '\n\n')
 	f.close()
